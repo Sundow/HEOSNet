11a841f4d5e65e7debd8d13a70e2d194f7434cb0
Artem Kliatchkine <A0081633@kiongroup.com>
2025-08-20 16:46:36 +0200

    Test client code cleanup.

--- HEOSNet.TestClient.WPF/MainWindow.xaml.cs
+++ HEOSNet.TestClient.WPF/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using HEOSNet;
-using System.Net;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Collections.ObjectModel;
-using System.Linq;
+using System.Net;
+using System.Windows;
 
 namespace HEOSNet.TestClient.WPF;
 
@@ -22,12 +9,11 @@ namespace HEOSNet.TestClient.WPF;
 /// </summary>
 public partial class MainWindow : Window
 {
-    private ObservableCollection<string> _discoveredDevices;
+    private ObservableCollection<string> _discoveredDevices = [];
 
     public MainWindow()
     {
         InitializeComponent();
-        _discoveredDevices = new ObservableCollection<string>();
         DiscoveredDevicesListBox.ItemsSource = _discoveredDevices;
     }
 
